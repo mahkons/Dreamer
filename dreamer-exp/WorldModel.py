68b4caf1e62d402a1349ab5f6bf6dfc1744cc6ca
Makhnev Konstantin <mah.kons@gmail.com>
2021-11-22 01:31:49 +0300

    remove embed detach

--- dreamer-exp/WorldModel.py
+++ dreamer-exp/WorldModel.py
@@ -49,7 +49,7 @@ class WorldModel():
         reconstruction = self.decoder(embed)
         rec_loss = F.mse_loss(obs, reconstruction)
         l2_reg_loss = REC_L2_REG * (embed ** 2).sum(dim=2).mean(dim=(0, 1))
-        embed = embed.detach()
+        #  embed = embed.detach() # detach or no detach
 
         init_hidden, prev_action = self.initial_state(batch_size)
         action = torch.cat([prev_action.unsqueeze(0), action], dim=0)
