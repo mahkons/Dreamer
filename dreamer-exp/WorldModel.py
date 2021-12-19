6ecc672431a67d9f36b4156b086a6707c89ddd59
Makhnev Konstantin <mah.kons@gmail.com>
2021-12-20 02:21:04 +0300

    update params

--- dreamer-exp/WorldModel.py
+++ dreamer-exp/WorldModel.py
@@ -11,7 +11,7 @@ from networks import RewardNetwork, DiscountNetwork, ObservationEncoder, Observa
 from models import RealNVP, MAF
 from params import STOCH_DIM, DETER_DIM, EMBED_DIM, MAX_KL, \
     MODEL_LR, GAMMA, MAX_GRAD_NORM, FROM_PIXELS, PREDICT_DONE, \
-    FLOW_GRU_DIM, FLOW_HIDDEN_DIM, FLOW_NUM_BLOCKS, FLOW_LOSS_COEFF, REC_L2_REG, MODEL_WEIGHT_DECAY
+    FLOW_GRU_DIM, FLOW_HIDDEN_DIM, FLOW_NUM_BLOCKS, REC_L2_REG, MODEL_WEIGHT_DECAY
 
 
 class WorldModel():
@@ -106,7 +106,7 @@ class WorldModel():
         return hidden_list, flow_list, jac_list
 
     def obs_step(self, embed, action, hidden):
-        embed_flow, logjac = self.flow_model.forward_flow(embed, torch.cat([hidden, action], dim=-1).detach())
+        embed_flow, logjac = self.flow_model.forward_flow(embed, torch.cat([hidden, action], dim=-1))
         hidden = self.transition_model(torch.cat([embed_flow, action], dim=-1).detach(), hidden)
         return hidden, embed_flow, logjac
         
