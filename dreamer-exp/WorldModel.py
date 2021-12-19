788d751c8547c946be712980a31ac89de2a2b20f
Makhnev Konstantin <mah.kons@gmail.com>
2021-12-20 02:57:18 +0300

    add ED model lr

--- dreamer-exp/WorldModel.py
+++ dreamer-exp/WorldModel.py
@@ -12,7 +12,7 @@ from models import RealNVP, MAF
 from params import STOCH_DIM, DETER_DIM, EMBED_DIM, MAX_KL, \
     MODEL_LR, GAMMA, MAX_GRAD_NORM, FROM_PIXELS, PREDICT_DONE, \
     FLOW_GRU_DIM, FLOW_HIDDEN_DIM, FLOW_NUM_BLOCKS, REC_L2_REG, MODEL_WEIGHT_DECAY, \
-    FLOW_LOSS_IN_GRU_MULTIPLIER
+    FLOW_LOSS_IN_GRU_MULTIPLIER, ED_MODEL_LR
 
 
 class WorldModel():
@@ -46,7 +46,7 @@ class WorldModel():
         )
 
         self.optimizer = torch.optim.Adam(self.parameters, lr=MODEL_LR, weight_decay=MODEL_WEIGHT_DECAY)
-        self.ed_optimizer = torch.optim.Adam(self.ed_parameters, lr=MODEL_LR, weight_decay=MODEL_WEIGHT_DECAY)
+        self.ed_optimizer = torch.optim.Adam(self.ed_parameters, lr=ED_MODEL_LR, weight_decay=MODEL_WEIGHT_DECAY)
 
         self.grad_multiplier = GradMultiplier(FLOW_LOSS_IN_GRU_MULTIPLIER) # for conditions
 
