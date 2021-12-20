033296ad18cdfe03945734425f626acf588a69a7
Makhnev Konstantin <mah.kons@gmail.com>
2021-12-20 03:17:46 +0300

    try with prior model

--- dreamer-exp/WorldModel.py
+++ dreamer-exp/WorldModel.py
@@ -12,7 +12,7 @@ from models import RealNVP, MAF
 from params import STOCH_DIM, DETER_DIM, EMBED_DIM, MAX_KL, \
     MODEL_LR, GAMMA, MAX_GRAD_NORM, FROM_PIXELS, PREDICT_DONE, \
     FLOW_GRU_DIM, FLOW_HIDDEN_DIM, FLOW_NUM_BLOCKS, REC_L2_REG, MODEL_WEIGHT_DECAY, \
-    FLOW_LOSS_IN_GRU_MULTIPLIER, ED_MODEL_LR
+    FLOW_LOSS_IN_GRU_MULTIPLIER, ED_MODEL_LR, WITH_PRIOR_MODEL
 
 
 class WorldModel():
@@ -209,6 +209,7 @@ class PriorModel(nn.Module):
 
     def forward(self, hidden):
         mu, log_std = self.model(hidden).chunk(2, dim=-1) 
+        if WITH_PRIOR_MODEL:
+            return torch.distributions.Normal(mu, torch.exp(log_std))
         return torch.distributions.Normal(torch.zeros_like(mu), torch.ones_like(log_std))
-        return torch.distributions.Normal(mu, torch.exp(log_std))
 
