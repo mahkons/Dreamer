8962fb6ab7aea4d158168ab095bed12925d8d872
Makhnev Konstantin <mah.kons@gmail.com>
2021-12-20 04:19:57 +0300

    add flow_loss coeff

--- dreamer-exp/WorldModel.py
+++ dreamer-exp/WorldModel.py
@@ -12,7 +12,7 @@ from models import RealNVP, MAF
 from params import STOCH_DIM, DETER_DIM, EMBED_DIM, MAX_KL, \
     MODEL_LR, GAMMA, MAX_GRAD_NORM, FROM_PIXELS, PREDICT_DONE, \
     FLOW_GRU_DIM, FLOW_HIDDEN_DIM, FLOW_NUM_BLOCKS, REC_L2_REG, MODEL_WEIGHT_DECAY, \
-    FLOW_LOSS_IN_GRU_MULTIPLIER, ED_MODEL_LR, WITH_PRIOR_MODEL
+    FLOW_LOSS_IN_GRU_MULTIPLIER, ED_MODEL_LR, WITH_PRIOR_MODEL, FLOW_LOSS_COEFF
 
 
 class WorldModel():
@@ -84,7 +84,7 @@ class WorldModel():
         flow_loss = -(prior.log_prob(flow_list).sum(dim=2) + jac_list).mean()
 
         self.optimizer.zero_grad()
-        (reward_loss + discount_loss + flow_loss).backward()
+        (reward_loss + discount_loss + flow_loss * FLOW_LOSS_COEFF).backward()
         nn.utils.clip_grad_norm_(self.parameters, MAX_GRAD_NORM)
         self.optimizer.step()
 
