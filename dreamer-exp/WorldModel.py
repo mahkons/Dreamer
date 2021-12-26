0449eca42b725d409de0ca7564c85130dc1d11cb
Makhnev Konstantin <mah.kons@gmail.com>
2021-12-27 00:11:33 +0300

    add another rec coeff

--- dreamer-exp/WorldModel.py
+++ dreamer-exp/WorldModel.py
@@ -12,7 +12,7 @@ from models import RealNVP, MAF
 from params import STOCH_DIM, DETER_DIM, EMBED_DIM, MAX_KL, \
     MODEL_LR, GAMMA, MAX_GRAD_NORM, FROM_PIXELS, PREDICT_DONE, \
     FLOW_GRU_DIM, FLOW_HIDDEN_DIM, FLOW_NUM_BLOCKS, REC_L2_REG, MODEL_WEIGHT_DECAY, \
-    FLOW_LOSS_IN_GRU_MULTIPLIER, ED_MODEL_LR, WITH_PRIOR_MODEL, FLOW_LOSS_COEFF
+    FLOW_LOSS_IN_GRU_MULTIPLIER, ED_MODEL_LR, WITH_PRIOR_MODEL, FLOW_LOSS_COEFF, ANOTHER_REC_COEFF
 
 
 class WorldModel(nn.Module):
@@ -91,7 +91,7 @@ class WorldModel(nn.Module):
         another_rec_loss = ((embed - another_rec)**2).sum(dim=2).mean()
 
         self.optimizer.zero_grad()
-        another_rec_loss.backward(retain_graph=True)
+        (ANOTHER_REC_COEFF * another_rec_loss).backward(retain_graph=True)
         self.flow_optimizer.zero_grad()
         (reward_loss + discount_loss + flow_loss * FLOW_LOSS_COEFF).backward()
         nn.utils.clip_grad_norm_(self.model_params, MAX_GRAD_NORM)
