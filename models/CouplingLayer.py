5d80f109ab47882c1a6ff57cf1bd246eddd7c337
Makhnev Konstantin <mah.kons@gmail.com>
2022-02-07 04:19:07 +0300

    modify Coupling layer

--- models/CouplingLayer.py
+++ models/CouplingLayer.py
@@ -10,29 +10,25 @@ class CouplingLayer(ConditionalFlow):
         assert(len(mask.shape) == 1)
 
         self.register_buffer("mask", mask)
-        #self.register_parameter("log_scale_scale", nn.Parameter(torch.tensor(0., dtype=torch.float)))
+        self.register_parameter("log_scale_scale", nn.Parameter(torch.tensor(0., dtype=torch.float)))
 
-        modules_scale = [nn.Linear(input_dim + condition_dim, hidden_shape), nn.ReLU()] \
-            + sum([[nn.Linear(hidden_shape, hidden_shape), nn.ReLU()] for _ in range(num_hidden)], []) \
-            + [nn.Linear(hidden_shape, input_dim)]
+        modules = [nn.utils.weight_norm(nn.Linear(input_dim + condition_dim, hidden_shape)), nn.ReLU()] \
+            + sum([[nn.utils.weight_norm(nn.Linear(hidden_shape, hidden_shape)), nn.ReLU()] for _ in range(num_hidden)], []) \
+            + [nn.utils.weight_norm(nn.Linear(hidden_shape, 2 * input_dim))]
 
-        modules_translate = [nn.Linear(input_dim + condition_dim, hidden_shape), nn.ReLU()] \
-            + sum([[nn.Linear(hidden_shape, hidden_shape), nn.ReLU()] for _ in range(num_hidden)], []) \
-            + [nn.Linear(hidden_shape, input_dim)]
+        self.model = nn.Sequential(*modules)
 
-        self.scale_net = nn.Sequential(*modules_scale, nn.Tanh())
-        self.translate_net = nn.Sequential(*modules_translate)
 
     def forward_flow(self, x, condition):
         masked_x = x * self.mask
         input = torch.cat([masked_x, condition], dim=-1)
-        log_s = self.scale_net(input)
-        t = self.translate_net(input)
+        log_s, t = self.model(input).chunk(2, dim=-1)
+        log_s = torch.tanh(log_s) * self.log_scale_scale
         return masked_x + (1 - self.mask) * (x * torch.exp(log_s) + t), (log_s * (1 - self.mask)).sum(dim=1)
 
     def inverse_flow(self, x, condition):
         masked_x = x * self.mask
         input = torch.cat([masked_x, condition], dim=-1)
-        log_s = self.scale_net(input)
-        t = self.translate_net(input)
+        log_s, t = self.model(input).chunk(2, dim=-1)
+        log_s = torch.tanh(log_s) * self.log_scale_scale
         return masked_x + (1 - self.mask) * ((x - t) * torch.exp(-log_s)), -(log_s * (1 - self.mask)).sum(dim=1)
