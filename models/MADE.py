91b00a63d83b2addccf78086180ec8e5f21568fc
Makhnev Konstantin <mah.kons@gmail.com>
2022-02-23 17:49:18 +0300

    fix default use_cond

--- models/MADE.py
+++ models/MADE.py
@@ -9,13 +9,16 @@ class MADE(ConditionalFlow):
         super(MADE, self).__init__()
 
         assert(hidden_dim >= dim > 2)
+        self.use_cond = use_cond
+        if not use_cond:
+            condition_dim = 0
 
         order_input = torch.arange(dim)
         order_hidden = torch.arange(hidden_dim) % (dim - 1)
         order_out = torch.arange(2 * dim) % dim
 
         self.model = nn.Sequential(
-            MaskedLinear(dim + condition_dim if use_cond else dim, hidden_dim, 
+            MaskedLinear(dim + condition_dim, hidden_dim, 
                 torch.cat([order_hidden[:, None] >= order_input[None],
                     torch.ones((hidden_dim, condition_dim), dtype=torch.bool, device=self.device)], dim=1)),
             nn.ELU(),
@@ -26,7 +29,7 @@ class MADE(ConditionalFlow):
         #self.log_scale_scale = nn.Parameter(torch.tensor(0., dtype=torch.float))
 
     def forward_flow(self, x, condition):
-        inputs = torch.cat([x, condition], dim=1) if use_cond else x
+        inputs = torch.cat([x, condition], dim=1) if self.use_cond else x
         log_s, t = self.model(inputs).chunk(2, dim=1)
         log_s = torch.tanh(log_s)
         return x * torch.exp(log_s) + t, log_s.sum(dim=1)
@@ -34,7 +37,7 @@ class MADE(ConditionalFlow):
     def inverse_flow(self, u, condition):
         x = torch.zeros_like(u)
         for i in range(0, x.shape[1]):
-            inputs = torch.cat([x, condition], dim=1) if use_cond else x
+            inputs = torch.cat([x, condition], dim=1) if self.use_cond else x
             log_s, t = self.model(inputs).chunk(2, dim=1)
             log_s = torch.tanh(log_s)
             x[:, i] = (u[:,i] - t[:,i]) * torch.exp(-log_s[:,i])
